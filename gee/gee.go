8e47e8a8b8120bbeb04987bafac5a8d6aed663bf
Jizishuo <jizishuo@qq.com>
2020-04-11 17:55:40 +0800

    group

--- gee/gee.go
+++ gee/gee.go
@@ -1,36 +1,66 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
 type HandlerFunc func(*Context)
 
+type RouterGroup struct {
+	prefix string
+	middlewares []HandlerFunc // support middleware
+	parent *RouterGroup   // support nesting
+	engine *Engine // all groups share a engine instance
+}
+
+
 type Engine struct {
+	*RouterGroup
 	router *router
+	groups []*RouterGroup // store all groups
 }
 
 func New() *Engine {
-	return &Engine{newRouter()}
+	engine := &Engine{router:newRouter()}
+	engine.RouterGroup = &RouterGroup{engine:engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	engine.router.addRouter(method, pattern, handler)
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix:      group.prefix+prefix,
+		parent:      group,
+		engine:      engine,
+	}
+	engine.groups =append(engine.groups, newGroup)
+	return newGroup
 }
 
-func (engine *Engine) GET (pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRouter(method, pattern, handler)
 }
 
-func (engine *Engine) POST (pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
+func (group *RouterGroup) GET (pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
 }
 
-func (engine *Engine) Run(addr string) (err error) {
-	return http.ListenAndServe(addr, engine)
+func (group *RouterGroup) POST (pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
+}
+
+func (group *RouterGroup) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, group)
 }
 
 func (engine *Engine) ServerHTTP(w http.ResponseWriter, req *http.Request) {
 	c := newContext(w, req)
 	engine.router.handle(c)
 }
+
+
+
