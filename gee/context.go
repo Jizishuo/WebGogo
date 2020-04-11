461d1ed3c093a63c0d8b4ab758b4494cf9427017
Jizishuo <jizishuo@qq.com>
2020-04-11 16:33:44 +0800

    router_2

--- gee/context.go
+++ gee/context.go
@@ -9,12 +9,22 @@ import (
 type H map[string]interface{}
 
 type Context struct {
+	// origin objects
 	Writer http.ResponseWriter
 	Req *http.Request
+	// request info
 	Path string
 	Method string
+	Params map[string]string
+	// response info
 	StatusCode int
 }
+
+func (c *Context) Param(key string) string {
+	value, _ := c.Params[key]
+	return value
+}
+
 
 func newContext(w http.ResponseWriter, req *http.Request) *Context {
 	return &Context{
--- gee/gee.go
+++ gee/gee.go
@@ -1,23 +1,21 @@
 package gee
 
 import (
-	"fmt"
 	"net/http"
 )
 
-type HandlerFunc func(http.ResponseWriter, *http.Request)
+type HandlerFunc func(*Context)
 
 type Engine struct {
-	router map[string]HandlerFunc
+	router *router
 }
 
 func New() *Engine {
-	return &Engine{make(map[string]HandlerFunc)}
+	return &Engine{newRouter()}
 }
 
 func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	key := method + "-" + pattern
-	engine.router[key] = handler
+	engine.router.addRouter(method, pattern, handler)
 }
 
 func (engine *Engine) GET (pattern string, handler HandlerFunc) {
@@ -33,10 +31,6 @@ func (engine *Engine) Run(addr string) (err error) {
 }
 
 func (engine *Engine) ServerHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUNED: %s\n", req.URL)
-	}
+	c := newContext(w, req)
+	engine.router.handle(c)
 }
