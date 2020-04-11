73e9a367227fd836646c8234fa94a52fd20523f8
Jizishuo <jizishuo@qq.com>
2020-04-11 20:54:03 +0800

    templates

--- gee/context.go
+++ gee/context.go
@@ -21,6 +21,9 @@ type Context struct {
 	// middleware
 	handlers []HandlerFunc
 	index int
+
+	// engine pointer
+	engine *Engine
 }
 
 func (c *Context) Param(key string) string {
@@ -77,16 +80,20 @@ func (c *Context) Data(code int, data []byte) {
 	c.Writer.Write(data)
 }
 
-func (c *Context) HTML(code int, html string) {
-	c.SetHeader("Content-Type", "text/html")
-	c.Status(code)
-	c.Writer.Write([]byte(html))
-}
+
 
 func (c *Context) Next() {
 	c.index ++
 	s := len(c.handlers)
 	for ; c.index< s; c.index++ {
 		c.handlers[c.index](c)
+	}
+}
+
+func (c *Context) HTML (code int, name string, data interface{}) {
+	c.SetHeader("Content-Type", "text/html")
+	c.Status(code)
+	if err := c.engine.htmlTemplates.ExecuteTemplate(c.Writer, name, data); err != nil {
+		c.Fail(500, err.Error())
 	}
 }
--- gee/gee.go
+++ gee/gee.go
@@ -1,8 +1,10 @@
 package gee
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path"
 	"strings"
 )
 
@@ -20,6 +22,8 @@ type Engine struct {
 	*RouterGroup
 	router *router
 	groups []*RouterGroup // store all groups
+	htmlTemplates *template.Template // for html render
+	funcMap template.FuncMap // for html render
 }
 
 func New() *Engine {
@@ -71,8 +75,36 @@ func (engine *Engine) ServerHTTP(w http.ResponseWriter, req *http.Request) {
 	}
 	c := newContext(w, req)
 	c.handlers = middlewares
+	c.engine = engine
 	engine.router.handle(c)
 }
 
+func (engine *Engine) SetFuncMap(funcMap template.FuncMap) {
+	engine.funcMap = funcMap
+}
+
+func (engine *Engine) LoadHTMLGlob(pattern string) {
+	engine.htmlTemplates = template.Must(template.New("").Funcs(engine.funcMap).ParseGlob(pattern))
+}
 
 
+func (group *RouterGroup) createStaticHandler(relativePath string, fs http.FileSystem) HandlerFunc {
+	absolutePath := path.Join(group.prefix, relativePath)
+	fileServer := http.StripPrefix(absolutePath, http.FileServer(fs))
+	return func(c *Context) {
+		file := c.Param("filepath")
+		// Check if file exists and/ or if we have permission to access it
+		if _, err := fs.Open(file); err != nil {
+			c.Status(http.StatusNotFound)
+			return
+		}
+		fileServer.ServeHTTP(c.Writer, c.Req)
+	}
+}
+
+// serve static files
+func (group *RouterGroup) Static(relativePath string, root string) {
+	handler := group.createStaticHandler(relativePath, http.Dir(root))
+	urlPattern := path.Join(relativePath, "/*filepath")
+	group.GET(urlPattern, handler)
+}
